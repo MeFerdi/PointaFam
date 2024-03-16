f44bebff5683eed0bd64db5349712283cf2e844c
MeFerdi <ferdinandodhis254@gmail.com>
2024-03-16 19:16:15 +0300

    added a submit form page

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'Agriculture Blog';
+export const SITE_DESCRIPTION =
+  'Articles, stories and tutorials from Agricultural firms and markets';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
